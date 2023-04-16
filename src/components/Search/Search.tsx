d904bdd455b14a20373f09368911fb4521dd1b02
Denis Savhenkov <tomrover@yandex.ru>
2023-04-16 12:01:38 +0300

    add debounce to search

--- src/components/Search/Search.tsx
+++ src/components/Search/Search.tsx
@@ -1,27 +1,41 @@
 import React, { useRef } from 'react';
 import styles from './Search.module.scss';
-import { useSelector } from 'react-redux';
-import { RootState, useAppDispatch } from '../../redux/store';
+import debounce from 'lodash.debounce';
+import { useAppDispatch } from '../../redux/store';
 import { setSearchContent } from '../../redux/slices/filterSlice';
 
 const Search: React.FC = () => {
-  const searchContent = useSelector(
-    (state: RootState) => state.filter.searchContent
-  );
+  const [localSearch, setLocalSeach] = React.useState<string>('');
   const dispatch = useAppDispatch();
   const searchRef = useRef<HTMLInputElement>(null);
 
+  //Clear Input
   const clearSeatchContent = () => {
     dispatch(setSearchContent(''));
+    setLocalSeach('');
     searchRef.current?.focus();
+  };
+
+  //debounce Function
+  const updatedInput = React.useCallback(
+    debounce((srt: string) => {
+      dispatch(setSearchContent(srt));
+    }, 500),
+    []
+  );
+
+  //Change content
+  const onChangeInput = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setLocalSeach(event.target.value);
+    updatedInput(event.target.value);
   };
 
   return (
     <div className={styles.search}>
       <input
         ref={searchRef}
-        value={searchContent}
-        onChange={(event) => dispatch(setSearchContent(event.target.value))}
+        value={localSearch}
+        onChange={onChangeInput}
         className={styles.input}
         type="text"
         placeholder="Найди свою пиццу..."
@@ -31,7 +45,7 @@ const Search: React.FC = () => {
         src="src/images/search.svg"
         alt="search"
       />
-      {searchContent && (
+      {localSearch && (
         <img
           onClick={clearSeatchContent}
           className={styles.cancel}
