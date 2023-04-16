8247f7dd24d170af4ac3c374c38b40659564b368
Denis Savhenkov <tomrover@yandex.ru>
2023-04-16 10:43:32 +0300

    add redux-persist - local storage

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,16 @@
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
-import { store } from './redux/store';
+import { persistor, store } from './redux/store';
 import { BrowserRouter } from 'react-router-dom';
+import { PersistGate } from 'redux-persist/integration/react';
 import App from './App';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <BrowserRouter>
     <Provider store={store}>
-      <App />
+      <PersistGate loading={null} persistor={persistor}>
+        <App />
+      </PersistGate>
     </Provider>
   </BrowserRouter>
 );
